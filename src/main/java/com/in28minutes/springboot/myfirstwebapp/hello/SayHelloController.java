1331a1b35037bb259e13a7bbd4c761289fd5b306
surajlunthi <whoissurajlunthi@gmail.com>
2022-12-21 22:28:21 +0530

    practice

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,45 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SayHelloController {
+
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My First HTML Page - Changed</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body - Changed");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
+	}
+	
+	//resources/META-INF/resources/WEB-INF/jsp/
+	// "say-hello-jsp" => sayHello.jsp 
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
+	@GetMapping("/")
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
+}
