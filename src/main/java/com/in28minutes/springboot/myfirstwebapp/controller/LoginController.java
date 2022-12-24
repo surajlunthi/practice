f7e1e5fd4b69f2824a1f1e181223f1372d79fedb
surajlunthi <whoissurajlunthi@gmail.com>
2022-12-24 11:41:03 +0530

    checkpoint created app working

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/controller/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/controller/LoginController.java
@@ -1,13 +1,22 @@
 package com.in28minutes.springboot.myfirstwebapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("login")
+    @RequestMapping(value = "login",method = RequestMethod.GET)
     public String loginPage(){
         return "login";
+    }
+    @RequestMapping(value = "login",method = RequestMethod.POST)
+    public String sayHello(@RequestParam String name, @RequestParam String password, ModelMap model){
+        model.put("name",name);
+        model.put("password",password);
+        return "welcome";
     }
 }
